e0c9a87041b439a199914dd85c67c86d6163ed40
zuxbrt <zmuhovic97@gmail.com>
2024-05-07 21:12:14 +0200

    rename app.css to styles.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app.css';
+import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/index';
